54d5fc250d72b7ab8524379bf9cd5cf71bebc022
2019-08-21 <nkleffne@janus13.cs.trinity.edu>
2022-10-25 16:22:26 -0500

    gaming

--- Reader.hs
+++ Reader.hs
@@ -1,6 +1,5 @@
 module Reader where
 import Data.Char
-import Data.List.Split
 
 type Terminal = String
 type NonTerminal = String
@@ -8,7 +7,7 @@ type Symbol = String
 type SymbolTable = [Symbol] --Map of strings to ints 
 data Token = SEMICOLON | DERIVES | ALSODERIVES | EPSILON | SYMBOL String deriving (Show, Eq)
 type Production = (NonTerminal, [Symbol])
-data IR = IR [Terminal] [NonTerminal] [(Integer, Production)]
+data IR = IR [Terminal] [NonTerminal] [(Integer, Production)] deriving (Show)
 
 isAlphaNumString str = foldl (\res char -> isAlphaNum char && res) True str
 
@@ -36,8 +35,10 @@ stringFromSymbol (SYMBOL str) = str
 
 --IR, symbol table, list of non-terminals You can move the generation of the symbol table to here if you want.
 grammarParse :: ([Token], SymbolTable) -> (IR, SymbolTable, [NonTerminal]) 
-grammarParse (tokenList, symbolTable) = (IR terminals nonTerminals productions, symbolTable, nonTerminals)
+grammarParse (tokenList, symbolTable) = (IR terminals nonTerminals productionz, symbolTable, nonTerminals)
     where (productions, nonTerminals) = buildProductionList tokenList
+          terminals = filter (\x -> not (elem x nonTerminals)) symbolTable
+          productionz = zip [1..] productions 
 
 buildProductionList :: [Token] -> ([Production],[NonTerminal])
 buildProductionList tokens =  (productionSet++productionList,nt:nonTerminals)
@@ -47,28 +48,28 @@ buildProductionList tokens =  (productionSet++productionList,nt:nonTerminals)
         (productionList,nonTerminals, tokens'') = helper tokens'
 
 buildProductionList' :: [Token] -> ([Production],[NonTerminal],[Token])
-buildProductionList' tokens =  (productionSet++productionList,nt:nonTerminals,tokens'')
+buildProductionList' [] = ([],[],[])
+buildProductionList' tokens@(SYMBOL _:_) =  (productionSet++productionList,nt:nonTerminals,tokens'')
   where (productionSet,nt,tokens') = buildProductionSet tokens
-        helper (SEMICOLON:tokens2) = buildProductionList' tokens2
+        helper (SEMICOLON:tokens2) = buildProductionList' tokens2 
         helper _ = error "Invalid production list'"
         (productionList, nonTerminals, tokens'') = helper tokens'
-buildProductionList' (EPSILON:tokens) = ([],nonTerminals,tokens)
-buildProductionList' _ = error "Invalid production list'"
+buildProductionList' toks = error $ show toks
 
 buildProductionSet :: [Token] -> ([Production],NonTerminal,[Token])
 buildProductionSet ((SYMBOL s):DERIVES:tokens) = (production:productionSet', s, tokens'')
   where (rhs, tokens') = buildRightHandSide tokens
         (productionSet', tokens'') = buildProductionSet' tokens' s
         production = (s,rhs)
-buildProductionSet _  = error "Invalid production set"
+buildProductionSet tokens  = error $ "Invalid production set on token: " ++ show (tokens)
 
 buildProductionSet' :: [Token] -> NonTerminal -> ([Production],[Token])
 buildProductionSet' (ALSODERIVES:tokens) nt = (production:productionSet', tokens'')
   where (rhs, tokens') = buildRightHandSide tokens
         (productionSet', tokens'') = buildProductionSet' tokens' nt
         production = (nt,rhs)
-buildProductionSet' (EPSILON:tokens) = ([],tokens)              
-buildProductionSet' _  = error "Invalid production set'"
+buildProductionSet' (SEMICOLON:tokens) nt = ([],SEMICOLON:tokens)              
+buildProductionSet' tokens nt = error $ "Invalid production set'" ++ show (tokens) ++ show nt
 
 buildRightHandSide :: [Token] -> ([Symbol],[Token])
 buildRightHandSide (EPSILON:tokens) = ([],tokens)
@@ -77,8 +78,8 @@ buildRightHandSide tokens = buildSymbolList tokens
 buildSymbolList' :: [Token] -> ([Symbol],[Token])
 buildSymbolList' ((SYMBOL s):tokens) = (s:symbolList,tokens')
   where (symbolList,tokens') = buildSymbolList' tokens
-buildSymbolList' (ALSODERIVES:tokens) = ([],tokens)
-buildSymbolList' (SEMICOLON:tokens) = ([],tokens)
+buildSymbolList' (ALSODERIVES:tokens) = ([],ALSODERIVES:tokens)
+buildSymbolList' (SEMICOLON:tokens) = ([],SEMICOLON:tokens)
 buildSymbolList' _ = error "Invalid symbol list'"
 
 buildSymbolList :: [Token] -> ([Symbol],[Token])
